685019615ff95334c07f75ac39817b20c8d6b0e0
DJ-kkk <129987327+DJ-kkk@users.noreply.github.com>
2024-01-17 18:18:49 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,52 @@
+img = "";
+status = "";
+function preload(){
+    img = loadImage('dog_cat.jpg')
+    }
+function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!")
+    status = true;
+    objectDetector.detect(img, gotResults);
+}
+function gotResults(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
+
+function draw() {
+    image(img, 0 , 0 ,640, 420);
+    text(objects[i].label)
+    (objects[i].confidence)
+    ( objects[i].x, objects[i].y);
+    objects[i].width
+    objects[i].height
+    
+    noFill();
+    stroke("red");
+    rect(30, 60, 450, 350);
+
+    fill("#FF0000")
+    text("Cat", 320, 120);
+    noFill();
+    stroke("#FF0000");
+    rect(300, 90, 270, 320 );
+
+
+    fill("red");
+    text("Cat", 320, 120);
+    noFill();
+    stroke("red");
+    rect(300, 90, 270, 320);
+
+}
